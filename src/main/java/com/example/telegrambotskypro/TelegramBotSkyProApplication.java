4e6a66a439e88d24349eb1f4f9c4b217b376d820
dbhor <dbhor1000@gmail.com>
2023-06-27 22:00:17 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotskypro/TelegramBotSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSkyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotSkyProApplication.class, args);
+    }
+
+}
